522207549e816bd465f08317ca7bd1a560a363be
pro-GRMR <prottoyrm@gmail.com>
2025-04-21 14:31:12 -0700

    tutorial

--- developed_lot.cpp
+++ developed_lot.cpp
@@ -3,37 +3,35 @@
 #include "developed_lot.hpp"
 #include <cstdio>
 #include <cassert>
-using namespace developed_lot
-;
+using namespace developed_lot;
 
-DevelopedLot::DevelopedLot():
-    Lot(0.0, 0.0, 0.0, 0.0), 
-    floor_area(0),
-    num_floors(0) {};
+DevelopedLot::DevelopedLot() : Lot(0.0, 0.0, 0.0, 0.0),
+                               floor_area(0),
+                               num_floors(0) {};
 
-DevelopedLot::DevelopedLot(double latitude, double longitude, double width, double length, double floor_area, int num_floors):
-    Lot(latitude, longitude, width,length),
-    floor_area(floor_area),
-    num_floors(num_floors)
-{};
+DevelopedLot::DevelopedLot(double latitude, double longitude, double width, double length, double floor_area, int num_floors) : Lot(latitude, longitude, width, length),
+                                                                                                                                floor_area(floor_area),
+                                                                                                                                num_floors(num_floors) {};
 
 DevelopedLot::~DevelopedLot() = default;
 
-double DevelopedLot::value() const{
+double DevelopedLot::value() const
+{
     return Lot::value() + (100 * this->floor_area * this->num_floors);
 };
 
-double DevelopedLot::floor_area_ratio() const{
+double DevelopedLot::floor_area_ratio() const
+{
     return (this->floor_area * this->num_floors) / this->land_area();
 };
 
-
-void developed_lot_test() {
+void developed_lot_test()
+{
     developed_lot::DevelopedLot dlot1 =
-    developed_lot::DevelopedLot(18.776, 108.5, 122.8, 19.1, 0, 0);
+        developed_lot::DevelopedLot(18.776, 108.5, 122.8, 19.1, 0, 0);
 
     developed_lot::DevelopedLot dlot2 =
-    developed_lot::DevelopedLot(-90.909090, 180.364, 6.8, 4.4, 60.1, 10);
+        developed_lot::DevelopedLot(-90.909090, 180.364, 6.8, 4.4, 60.1, 10);
 
     double val1 = dlot1.value();
     std::cout << "Val1:" << val1 << std::endl;
@@ -42,9 +40,15 @@ void developed_lot_test() {
 
     assert(val1 == 37527.68);
     assert(val2 == 60578.72);
-
 };
 
-int main() {
+Office::Office(double a, double b, double c, double d, double e, int f, int g) : DevelopedLot(a, b, c, d, e, f), num_of_offices(g) {}
+double Office::value() const
+{
+    return DevelopedLot::value() + 200 * num_of_offices;
+}
+
+int main()
+{
     developed_lot_test();
 };
--- developed_lot.hpp
+++ developed_lot.hpp
@@ -2,20 +2,30 @@
 #include "lot.cpp"
 #pragma once
 
-namespace developed_lot {
-class DevelopedLot : public Lot{
-private:
-    double floor_area;
-    double num_floors;
+namespace developed_lot
+{
+    class DevelopedLot : public Lot
+    {
+    private:
+        double floor_area;
+        double num_floors;
 
-public:
-    DevelopedLot();
-    DevelopedLot(double, double, double, double, double, int);
-    virtual ~DevelopedLot();
-    double virtual value() const;
+    public:
+        DevelopedLot();
+        DevelopedLot(double, double, double, double, double, int);
+        virtual ~DevelopedLot();
+        double virtual value() const;
 
+    protected:
+        double floor_area_ratio() const;
+    };
+    class Office : public DevelopedLot
+    {
+    private:
+        int num_of_floors;
 
-protected:
-    double floor_area_ratio() const;
-}; 
+    public:
+        Office(double, double, double, double, double, int, int);
+        double virtual value() const;
+    }
 };
